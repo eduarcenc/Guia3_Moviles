dd822c6dfcb0ec295cb7adeb62d384e7d7571f19
Eduardo <jose162911@gmail.com>
2018-10-01 15:51:14 -0600

    lab
    code

---
+++ app/src/main/java/org/dev4u/hv/guia3_ejemplo/Contacto.java
@@ -0,0 +1,15 @@
+package org.dev4u.hv.guia3_ejemplo;
+
+/**
+ * Created by eduardo on 30/9/18.
+ */
+
+public class Contacto {
+    public String nombre;
+    public String numero;
+
+    public Contacto(String nombre, String numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+}
---
+++ app/src/main/java/org/dev4u/hv/guia3_ejemplo/Mensaje.java
@@ -0,0 +1,15 @@
+package org.dev4u.hv.guia3_ejemplo;
+
+/**
+ * Created by eduardo on 30/9/18.
+ */
+
+public class Mensaje {
+    public String fecha;
+    public String contenido;
+
+    public Mensaje(String fecha, String contenido) {
+        this.fecha = fecha;
+        this.contenido = contenido;
+    }
+}
